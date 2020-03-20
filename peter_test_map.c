90e61f27984e9a09c1b1a0a9c5fc1fcb469468e7
Akpabio Peter Ekpewoh <akekp1@morgan.edu>
2020-03-20 15:23:33 -0400

    The file maps the pointer as required. unable to print out though

--- peter_test_map.c
+++ peter_test_map.c
@@ -2,7 +2,7 @@
 #include <inttypes.h>
 #include "map.h"
 
-
+/*
 char ConvertToString(uint8_t key[],char usr_pin_str[])
 {
     uint8_t * ptr= key;
@@ -22,36 +22,27 @@ int ConvertToUint8() //after the characterstring is stored when required to prin
 {
 
 }
-
-int mp_data(char * user_name, char * user_str)
+*/
+int mp_data(char * user_name, uint8_t key[])
 {
-    //typedef map_t(unsigned char *) uint_map_t;
-    map_str_t usr_data;
+    typedef map_t(uint8_t *) uint_map_t;
+    uint_map_t usr_data;
     map_init(&usr_data);
-    //printf("I am here\n");
- //   printf("%s\t",user_name);
-  /*
-    for(int i=0;i<3;i++)
+
+    uint8_t * ptr = key;
+    printf("%d\n",sizeof(ptr));
+    for (int i=0; i<3; i++)
     {
-    printf("%x", user_pin [i]);
+    printf("%x\n", ptr[i]);
     }
-    
-    printf("\n");*/
 
-    map_set(&usr_data, user_name, user_str);
-
-    
-
-    // uint_map_t shared_data;
-    // map_init(&shared_data);
-
-    // map_set(&shared_data, user_name, 0x25);
-   
-    char* val = map_get(&usr_data, user_name);
+    map_set(&usr_data, user_name, ptr);
+ 
+    uint8_t val = map_get(&usr_data, user_name);
 
     for (int i=0; i<3; i++)
     {
-        printf("%s", *val);
+        printf("%x", val);
     }
     printf("\n");
 /*  }
@@ -65,28 +56,16 @@ int mp_data(char * user_name, char * user_str)
     map_deinit(&usr_data);
 }
 
-
-
 int main()
 {
    char user_name [] = "James";
-   int n = 3;
-   const uint8_t user_pin [3] = {0x23, 0x46, 0x25};
-   char user_str [3];
-   user_str[n] = '\0';
-  // int n = 3; 
+   uint8_t user_pin [] = {0x23, 0x25, 0x27};
 /*
-   for (int i=0;i<3;i++)
+   for (int i=0; i<3; i++)
    {
-   printf("%x",user_pin[i]);
+   printf("%x", user_pin[i]);
    }
-   printf("\n");*/
-   
-    ConvertToString(user_pin, user_str);
- //   printf("size of str: %d\n",strlen(user_str));
-    printf("%s\n", user_str);
-
-    //printf("Done\r\n");
-    mp_data(&user_name, &user_str);
+   */
+   mp_data(&user_name, user_pin);
    return 0;
 }
